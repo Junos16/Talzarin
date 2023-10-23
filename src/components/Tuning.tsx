83f6de4141a8fe4b3779a2c306775a3da1c629fe
Junos16 <hriddhitdatta2002@gmail.com>
2023-10-23 18:14:31 +0530

    finished very basic element for edo tuning

--- src/components/Tuning.tsx
+++ src/components/Tuning.tsx
@@ -1,32 +1,43 @@
-import React from "react";
-import { useState } from "react";
-
-function TuningInput() {
-    const [inputValue, setInputValue] = useState('');
-    const handleInputChange = ()
-    return (
-        <div>
-            <label>Enter cents for tuning</label>
-            <input
-                type = "text"
-                value = {inputValue}
-                onChange = {handleInputChange}
-            />
-        </div>
-    )
-};
-
-function TuningOutput() {
-
-};
+import React, { useState } from 'react';
 
 function Tuning() {
-    return (
-        <>
-        <TuningInput></TuningInput>
-        <TuningOutput></TuningOutput>
-        </>
-    )
-};
+  const [division, setDivision] = useState(12); // Default to 12 divisions
+  const [centValues, setCentValues] = useState([]);
+
+  const calculateCentValues = () => {
+    const centStep = 1200 / division;
+    const centArray = [];
+
+    for (let i = 0; i < division; i++) {
+      const centValue = i * centStep;
+      centArray.push(centValue);
+    }
+
+    setCentValues(centArray);
+  };
+
+  return (
+    <div>
+      <label>Enter the number of divisions of an octet (EDO):</label>
+      <input
+        type="number"
+        value={division}
+        onChange={(e) => setDivision(e.target.value)}
+      />
+      <button onClick={calculateCentValues}>Calculate Cent Values</button>
+      <div>
+        <h2>Cent Values:</h2>
+        <ul>
+          {centValues.map(cent => (
+            <tr>
+              <td>{cent}</td>
+              <td>{cent*(256/1200)}</td>
+            </tr> 
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
 
 export default Tuning;

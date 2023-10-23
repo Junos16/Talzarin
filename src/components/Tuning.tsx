0cdf20fbb9b8b72bd8e477678a9c6a932c1791d1
Junos16 <hriddhitdatta2002@gmail.com>
2023-10-23 19:11:49 +0530

    added column to show color corresponding to interval

--- src/components/Tuning.tsx
+++ src/components/Tuning.tsx
@@ -1,19 +1,20 @@
 import React, { useState } from 'react';
 
 function Tuning() {
-  const [division, setDivision] = useState(12); // Default to 12 divisions
-  const [centValues, setCentValues] = useState([]);
+  const [division, setDivision] = useState(12);
+  const [tuningData, setTuningData] = useState([]);
 
-  const calculateCentValues = () => {
+  const calculateTuningData = () => {
     const centStep = 1200 / division;
-    const centArray = [];
+    const hues = Array.from({ length: division }, (_, i) => Math.round((i * 360) / division));
 
-    for (let i = 0; i < division; i++) {
-      const centValue = i * centStep;
-      centArray.push(centValue);
-    }
+    const data = Array.from({ length: division }, (_, i) => ({
+      index: i + 1,
+      centValue: i * centStep,
+      hue: hues[i],
+    }));
 
-    setCentValues(centArray);
+    setTuningData(data);
   };
 
   return (
@@ -24,17 +25,34 @@ function Tuning() {
         value={division}
         onChange={(e) => setDivision(e.target.value)}
       />
-      <button onClick={calculateCentValues}>Calculate Cent Values</button>
+      <button onClick={calculateTuningData}>Generate Tuning</button>
       <div>
-        <h2>Cent Values:</h2>
-        <ul>
-          {centValues.map(cent => (
+        <table>
+          <thead>
             <tr>
-              <td>{cent}</td>
-              <td>{cent*(256/1200)}</td>
-            </tr> 
-          ))}
-        </ul>
+              <th>Index</th>
+              <th>Cent Value</th>
+              <th>Color</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tuningData.map((item) => (
+              <tr key={item.index}>
+                <td>{item.index}</td>
+                <td>{item.centValue}</td>
+                <td>
+                  <div
+                    style={{
+                      width: '30px',
+                      height: '30px',
+                      backgroundColor: `hsl(${item.hue}, 100%, 50%)`,
+                    }}
+                  ></div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
